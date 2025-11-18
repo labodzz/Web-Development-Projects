aa2210b0bf9be624051635f64d8e44e591eaf636
labodzz <lbojic1@etf.unsa.ba>
2025-11-18 21:16:44 +0100

    dodatak editor

--- editor.js
+++ editor.js
@@ -1,21 +1,10 @@
-import EditorTeksta from "./EditorTeksta.js";
+import EditorTeksta from "EditorTeksta.js";
 
 // Dohvat div-a
-let div = document.querySelector(".naslov");
+let div = document.getElementsByClassName("sadrzaj");
 let editor = EditorTeksta(div);
 
-window.addEventListener("DOMContentLoaded", () => {
-  let div = document.querySelector(".naslov");
-  let btn = document.getElementByClassName("dugme1");
-
-  let editor = EditorTeksta(div);
-  let rez = {};
-
-  btn.addEventListener("click", () => {
-    rez = editor.dajBrojRijeci();
-  });
-
-  document.querySelector(
-    ".rezultati"
-  ).innerText = `Broj riječi: ${rez.brojRijeci}, Broj znakova: ${rez.brojZnakova}`;
+let btn = document.getElementById("brojRijeciBtn");
+btn.addEventListener("click", () => {
+  alert("Broj riječi: " + editor.dajBrojRijeci());
 });
