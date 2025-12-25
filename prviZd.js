0ef05bf30a5d9ca75d8349e6791c8c5ef7471577
labodzz <lbojic1@etf.unsa.ba>
2025-12-25 15:03:49 +0100

    test html

--- prviZd.js
+++ prviZd.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 const fs = require("fs");
 const path = require("path");
