ac8c72da27062fc986b7dac101972b3b1703dff8
labodzz <lbojic1@etf.unsa.ba>
2025-12-26 16:19:49 +0100

    modul zavrsen, dodan editor.html

--- PoziviAjax.js
+++ PoziviAjax.js
@@ -43,19 +43,103 @@ const PoziviAjax = (function () {
       ajax.send(JSON.stringify({ userId: userId }));
     },
     updateLine: function (scenarioId, lineId, userId, newText, callback) {
-      /* ... */
+      let ajax = new XMLHttpRequest();
+      ajax.open("PUT", `/api/scenarios/${scenarioId}/lines/${lineId}`, true);
+      ajax.setRequestHeader("Content-Type", "application/json");
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+          let response = null;
+          try {
+            response = JSON.parse(ajax.responseText);
+          } catch (e) {
+            response = null;
+          }
+          callback(ajax.status, response);
+        }
+      };
+      ajax.send(JSON.stringify({ userId: userId, newText: newText }));
     },
     lockCharacter: function (scenarioId, characterName, userId, callback) {
-      /* ... */
+      let ajax = new XMLHttpRequest();
+      ajax.open(
+        "POST",
+        `/api/scenarios/${scenarioId}/characters/${encodeURIComponent(
+          characterName
+        )}/lock`,
+        true
+      );
+      ajax.setRequestHeader("Content-Type", "application/json");
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+          let response = null;
+          try {
+            response = JSON.parse(ajax.responseText);
+          } catch (e) {
+            response = null;
+          }
+          callback(ajax.status, response);
+        }
+      };
+      ajax.send(JSON.stringify({ userId: userId }));
     },
     updateCharacter: function (scenarioId, userId, oldName, newName, callback) {
-      /* ... */
+      let ajax = new XMLHttpRequest();
+      ajax.open(
+        "PUT",
+        `/api/scenarios/${scenarioId}/characters/${encodeURIComponent(
+          oldName
+        )}`,
+        true
+      );
+      ajax.setRequestHeader("Content-Type", "application/json");
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+          let response = null;
+          try {
+            response = JSON.parse(ajax.responseText);
+          } catch (e) {
+            response = null;
+          }
+          callback(ajax.status, response);
+        }
+      };
+      ajax.send(JSON.stringify({ userId: userId, newName: newName }));
     },
     getDeltas: function (scenarioId, since, callback) {
-      /* ... */
+      let ajax = new XMLHttpRequest();
+      ajax.open(
+        "GET",
+        `/api/scenarios/${scenarioId}/deltas?since=${since}`,
+        true
+      );
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+          let response = null;
+          try {
+            response = JSON.parse(ajax.responseText);
+          } catch (e) {
+            response = null;
+          }
+          callback(ajax.status, response);
+        }
+      };
+      ajax.send();
     },
     getScenario: function (scenarioId, callback) {
-      /* ... */
+      let ajax = new XMLHttpRequest();
+      ajax.open("GET", `/api/scenarios/${scenarioId}`, true);
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+          let response = null;
+          try {
+            response = JSON.parse(ajax.responseText);
+          } catch (e) {
+            response = null;
+          }
+          callback(ajax.status, response);
+        }
+      };
+      ajax.send();
     },
   };
 })();
